5650e10dee178337448db7d0c7bb4c29176b7988
Benedikt Schnatterbeck <benediktms@hey.com>
2023-08-29 13:18:26 +0100

    implement alternative attempt to correctly map json values back to keys
    Also implement caching

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,71 @@ extern crate serde_json;
 
 use chrono::Utc;
 use dotenv::dotenv;
+use serde_json::json;
 use serde_json::Error as JsonError;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
 use std::path::Path;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let api_key = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY must be set");
+    let target_lang = env::var("TARGET_LANG").expect("TARGET_LANG must be set");
+
+    let cache_path = "data/cache.json";
+    let mut cache: HashMap<String, String> = match fs::read_to_string(cache_path) {
+        Ok(contents) => serde_json::from_str(&contents).unwrap_or_default(),
+        Err(_) => HashMap::new(),
+    };
+
+    let suffix = "::";
+
+    // Read the JSON file
+    let file_path = Path::new("data/input.json");
+    let json_value: Value = read_json(file_path)?;
+
+    // Collect values to translate
+    let mut values_to_translate = Vec::new();
+    collect_values(&json_value, &mut values_to_translate, "");
+
+    let mut flat_map = HashMap::new();
+    collapse_json_structure(&json_value, &mut flat_map, "");
+
+    // Translate the values
+    let translated_values = translate_values(
+        &values_to_translate,
+        &api_key,
+        &target_lang,
+        suffix,
+        &mut cache,
+    )
+    .await?;
+
+    // Update the flat map with the translated values
+    for (key, value) in &mut flat_map {
+        if let Some(translated_value) = translated_values.get(key) {
+            *value = json!(translated_value);
+        }
+    }
+
+    // Reconstruct the JSON with translated values
+    let translated_json = rebuild_json_structure(&flat_map);
+
+    // Write the translated JSON to a new file
+    let output_file_path = format!("data/{}_{}.json", Utc::now().timestamp(), target_lang);
+    write_json(Path::new(&output_file_path), &translated_json)?;
+
+    let cache_json = json!(cache);
+    fs::write(cache_path, cache_json.to_string())?;
+
+    Ok(())
+}
 
 fn read_json<P: AsRef<Path>>(path: P) -> Result<Value, JsonError> {
     let mut file = File::open(path).map_err(JsonError::io)?;
@@ -28,40 +85,83 @@ fn write_json<P: AsRef<Path>>(path: P, value: &Value) -> io::Result<()> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let api_key = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY must be set");
-    let target_lang = env::var("TARGET_LANG").expect("TARGET_LANG must be set");
-    let suffix = "::";
+fn collapse_json_structure(
+    json_value: &Value,
+    flat_map: &mut HashMap<String, Value>,
+    prefix: &str,
+) {
+    match json_value {
+        Value::Object(map) => {
+            for (key, value) in map {
+                let new_prefix = if prefix.is_empty() {
+                    key.clone()
+                } else {
+                    format!("{}->{}", prefix, key)
+                };
+                collapse_json_structure(value, flat_map, &new_prefix);
+            }
+        }
+        Value::Array(arr) => {
+            for (index, value) in arr.iter().enumerate() {
+                let new_prefix = format!("{}[{}]", prefix, index);
+                collapse_json_structure(value, flat_map, &new_prefix);
+            }
+        }
+        _ => {
+            flat_map.insert(prefix.to_string(), json_value.clone());
+        }
+    }
+}
 
-    // Read the JSON file
-    let file_path = Path::new("data/input.json");
-    let json_value: Value = read_json(file_path)?;
+fn insert_into_json(target: &mut Value, keys: &[&str], value: &Value) {
+    if keys.is_empty() {
+        return;
+    }
 
-    // Collect values to translate
-    let mut values_to_translate = Vec::new();
-    collect_values(&json_value, &mut values_to_translate, "");
+    let (first, rest) = keys.split_first().unwrap();
 
-    // Translate the values
-    let translated_values =
-        translate_values(&values_to_translate, &api_key, &target_lang, suffix).await?;
+    if rest.is_empty() {
+        if let Value::Object(ref mut map) = target {
+            map.insert(first.to_string(), value.clone());
+        }
+    } else {
+        let next_target = match target.get_mut(*first) {
+            Some(next_target) => next_target,
+            None => {
+                target[*first] = json!({});
+                target.get_mut(*first).unwrap()
+            }
+        };
 
-    // Reconstruct the JSON with translated values
-    let translated_json = translate_json_values(&json_value, &translated_values, "", suffix)?;
+        if next_target.is_object() {
+            insert_into_json(next_target, rest, value);
+        } else {
+            *next_target = json!({});
+            insert_into_json(next_target, rest, value);
+        }
+    }
+}
 
-    // Write the translated JSON to a new file
-    let output_file_path = format!("data/{}_{}.json", Utc::now().timestamp(), target_lang);
-    write_json(Path::new(&output_file_path), &translated_json)?;
+fn rebuild_json_structure(flat_map: &HashMap<String, Value>) -> Value {
+    let mut json_value = json!({});
 
-    Ok(())
+    for (key, value) in flat_map {
+        let keys: Vec<&str> = key.split("->").collect();
+        insert_into_json(&mut json_value, &keys, value);
+    }
+
+    json_value
 }
 
 fn collect_values(json_value: &Value, values: &mut Vec<(String, String)>, prefix: &str) {
     match json_value {
         Value::Object(map) => {
             for (key, value) in map {
-                let new_prefix = format!("{}{}", prefix, key);
+                let new_prefix = if prefix.is_empty() {
+                    key.clone()
+                } else {
+                    format!("{}->{}", prefix, key)
+                };
                 collect_values(value, values, &new_prefix);
             }
         }
@@ -72,8 +172,7 @@ fn collect_values(json_value: &Value, values: &mut Vec<(String, String)>, prefix
             }
         }
         Value::String(s) => {
-            let full_key = format!("{}{}", prefix, s); // Include both the JSON key and the JSON string value
-            values.push((full_key, s.clone()));
+            values.push((prefix.to_string(), s.clone()));
         }
         _ => {}
     }
@@ -84,6 +183,7 @@ async fn translate_values(
     api_key: &str,
     target_lang: &str,
     suffix: &str,
+    cache: &mut HashMap<String, String>,
 ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let mut translated = HashMap::new();
     let mut batch = String::new();
@@ -91,11 +191,19 @@ async fn translate_values(
     let mut keys_for_batch = Vec::new();
 
     for (key, value) in values {
+        // Check cache first
+        if let Some(cached_translation) = cache.get(value) {
+            println!("Cache hit for value: {}", value);
+            translated.insert(key.clone(), cached_translation.clone());
+            continue;
+        }
+
         let new_length = batch_length + value.len() + suffix.len();
         if new_length > 1500 {
             // Translate the current batch
             let translated_batch =
-                translate_batch(&batch, &keys_for_batch, api_key, target_lang, suffix).await?;
+                translate_batch(&batch, &keys_for_batch, api_key, target_lang, suffix, cache)
+                    .await?;
             translated.extend(translated_batch);
 
             // Reset the batch and keys_for_batch
@@ -114,7 +222,7 @@ async fn translate_values(
     // Translate the remaining batch
     if !batch.is_empty() {
         let translated_batch =
-            translate_batch(&batch, &keys_for_batch, api_key, target_lang, suffix).await?;
+            translate_batch(&batch, &keys_for_batch, api_key, target_lang, suffix, cache).await?;
         translated.extend(translated_batch);
     }
 
@@ -127,8 +235,31 @@ async fn translate_batch(
     api_key: &str,
     target_lang: &str,
     suffix: &str,
+    cache: &mut HashMap<String, String>,
 ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     let mut translated = HashMap::new();
+
+    // Check cache first
+    let mut all_cached = true;
+    for key in keys_for_batch.iter() {
+        println!("key: {}", key);
+
+        if let Some(cached_translation) = cache.get(key) {
+            println!("Cache hit for key: {}", key);
+            translated.insert(key.clone(), cached_translation.clone());
+        } else {
+            println!("Cache miss for key: {}", key);
+            all_cached = false;
+            break;
+        }
+    }
+
+    // If all translations are cached, return early
+    if all_cached {
+        return Ok(translated);
+    }
+
+    // Otherwise, proceed with API call
     let client = reqwest::Client::new();
     let params = [("text", batch), ("target_lang", target_lang)];
 
@@ -141,66 +272,20 @@ async fn translate_batch(
 
     if res.status().is_success() {
         let body = res.text().await?;
-
-        if body.is_empty() {
-            return Err("Empty response from DeepL API".into());
-        }
-
         let json: Value = serde_json::from_str(&body)?;
         let translated_text = json["translations"][0]["text"].as_str().unwrap_or(batch);
 
         // Split the translated_text back into individual strings based on the suffix
         let translated_values: Vec<&str> = translated_text.split(suffix).collect();
 
-        // Map them back to their original keys
+        // Map them back to their original keys and update the cache
         for (key, trans) in keys_for_batch.iter().zip(translated_values.iter()) {
             translated.insert(key.clone(), trans.to_string());
+            cache.insert(key.clone(), trans.to_string()); // Update the cache
         }
     } else {
         return Err(format!("Received a {} from DeepL API", res.status()).into());
     }
 
-    println!("Translated batch: {:?}", translated);
-
     Ok(translated)
-}
-
-fn translate_json_values(
-    json_value: &Value,
-    translated_values: &HashMap<String, String>,
-    prefix: &str,
-    suffix: &str,
-) -> Result<Value, Box<dyn std::error::Error>> {
-    match json_value {
-        Value::Object(map) => {
-            let mut new_map = serde_json::Map::new();
-            for (key, value) in map {
-                let new_prefix = format!("{}{}", prefix, key);
-                let new_value =
-                    translate_json_values(value, translated_values, &new_prefix, suffix)?;
-                new_map.insert(key.clone(), new_value);
-            }
-            Ok(Value::Object(new_map))
-        }
-        Value::Array(arr) => {
-            let new_arr: Result<Vec<_>, _> = arr
-                .iter()
-                .map(|v| translate_json_values(v, translated_values, prefix, suffix))
-                .collect();
-            Ok(Value::Array(new_arr?))
-        }
-        Value::String(s) => {
-            let full_key = format!("{}{}", prefix, s); // Use the full key including the prefix
-            let translated_value = translated_values.get(&full_key).unwrap_or(s).clone();
-
-            // Remove the suffix from the translated value
-            let mut final_translated_value = translated_value;
-            if final_translated_value.ends_with(suffix) {
-                final_translated_value.truncate(final_translated_value.len() - suffix.len());
-            }
-
-            Ok(Value::String(final_translated_value))
-        }
-        _ => Ok(json_value.clone()),
-    }
 }
